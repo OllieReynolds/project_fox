46ead7f2ddcabdcf886c188d8e6bc25b15e8f101
OllieReynolds <cubed@outlook.com>
2023-07-17 12:27:05 +0100

    Stats degrading

--- client/App.js
+++ client/App.js
@@ -1,16 +1,25 @@
-import 'react-native-gesture-handler';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import MainScreen from './screens/MainScreen';
+import StatsScreen from './screens/StatsScreen';
+import { StatsContext } from './StatsContext';
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Main">
-        <Stack.Screen name="Main" component={MainScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    const [hunger, setHunger] = React.useState(1.0);
+    const [happiness, setHappiness] = React.useState(1.0);
+    const [health, setHealth] = React.useState(1.0);
+
+    return (
+        <StatsContext.Provider value={{ hunger, setHunger, happiness, setHappiness, health, setHealth }}>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="Main">
+                    <Stack.Screen name="Main" component={MainScreen} />
+                    <Stack.Screen name="Stats" component={StatsScreen} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </StatsContext.Provider>
+    );
 }
--- client/screens/MainScreen.js
+++ client/screens/MainScreen.js
@@ -1,35 +1,66 @@
-import React from 'react';
-import { View, StyleSheet, Image } from 'react-native';
-import { Button } from 'react-native-paper';
-
+import React, { useEffect } from 'react';
+import { View, StyleSheet, Image, Button } from 'react-native';
+import { Button as PaperButton } from 'react-native-paper';
 import foxImage from '../assets/f_normal.png';
+import { StatsContext } from '../StatsContext';
 
-export default function MainScreen() {
-  return (
-    <View style={styles.container}>
-      <Image source={foxImage} style={styles.image} />
-      <Button mode="contained" style={styles.button}>
-        Feed Fox
-      </Button>
-      <Button mode="contained" style={styles.button}>
-        Play With Fox
-      </Button>
-    </View>
-  );
+export default function MainScreen({ navigation }) {
+    const { hunger, setHunger, happiness, setHappiness, health, setHealth } = React.useContext(StatsContext);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setHunger(hunger => Math.max(hunger - 0.07, 0));
+            setHappiness(happiness => Math.max(happiness - 0.1, 0));
+            setHealth(health => Math.max(health - 0.025, 0));
+        }, 5000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <Image source={foxImage} style={styles.image} />
+            <PaperButton
+                mode='contained'
+                style={styles.button}
+                onPress={() => setHunger(1.0)}
+            >
+                Feed Fox
+            </PaperButton>
+            <PaperButton
+                mode='contained'
+                style={styles.button}
+                onPress={() => setHappiness(1.0)}
+            >
+                Play With Fox
+            </PaperButton>
+            <PaperButton
+                mode='contained'
+                style={styles.button}
+                onPress={() => setHealth(1.0)}
+            >
+                Boost Fox Health
+            </PaperButton>
+            <Button
+                title='See Stats'
+                onPress={() => navigation.navigate('Stats')}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  image: {
-    width: 200,
-    height: 200,
-  },
-  button: {
-    margin: 10,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    image: {
+        width: 200,
+        height: 200,
+        marginBottom: 20
+    },
+    button: {
+        width: 200,
+        marginBottom: 10
+    }
 });
