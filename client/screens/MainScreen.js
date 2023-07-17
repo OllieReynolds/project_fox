7fed52d97b09e2be4c1d39de9e839b6e0bd0d510
OllieReynolds <cubed@outlook.com>
2023-07-17 11:33:21 +0100

    Basic screen working

--- client/screens/MainScreen.js
+++ client/screens/MainScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, StyleSheet, Image } from 'react-native';
 import { Button } from 'react-native-paper';
 
-import foxImage from '../assets/fox.png';
+import foxImage from '../assets/f_normal.png';
 
 export default function MainScreen() {
   return (
